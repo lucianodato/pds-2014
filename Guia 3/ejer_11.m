a08426e98d2edbc0df71b60722acca25f0b045d7
Luciano <lucianodato@gmail.com>
2014-04-09 11:20:01 -0300

    Comentado el resultado del 11

--- Guia 3/ejer_11.m
+++ Guia 3/ejer_11.m
@@ -32,8 +32,8 @@ N1=length(f1);
 %Pongo en 0 todas las frecuencias que no sean LA o armonicos de LA1
 %Solo considere 2 armonicos de LA
 %Igual tendria que usar un umbral de amplitud
-umbral = 150;
-f1=abs(f1);
+umbral = 200;
+f1=abs(f1);%Esto es para que no explote
 for i = 1:N1
 	if(i != 2778 && i != 8290 && i != 1389 && i != 6901 && f1(i) < umbral)
 		f1(i) = 0;
@@ -48,8 +48,8 @@ n1la=real(ifft(f1));
 
 subplot(2,2,4)
 plot(n1la);
-
-
+%Es muy malo el metodo, El ruido blanco tambien tiene la nota LA por lo tanto la reconoce tambien en el filtrado
+%Pero la primera y la ultima nota que son las que importaba las dejo pasar
 #wavwrite(n1la,11025,32,"Señal_Filtrada.wav");
 #wavwrite(n1,11025,32,"Señal.wav");
 
